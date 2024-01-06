447428290a6870d875d0fb720bd4d064d4a270cf
sagomanoj <sago.manoj@gmail.com>
2024-01-06 20:14:11 +0530

    Added Production appsettings

--- Mango.GatewaySolution/Program.cs
+++ Mango.GatewaySolution/Program.cs
@@ -1,14 +1,23 @@
+using Mango.GatewaySolution.Extensions;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Values;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddOcelot();
+if (builder.Environment.EnvironmentName.ToString().ToLower().Equals("production"))
+{
+    builder.Configuration.AddJsonFile("ocelot.Production.json", optional: false, reloadOnChange: true);
+}
+else
+{
+    builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+}
+builder.Services.AddOcelot(builder.Configuration);
 
-
+builder.AddAppAuthetication();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.UseOcelot();
+app.UseOcelot().GetAwaiter().GetResult();
 app.Run();
